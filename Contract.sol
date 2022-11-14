ca84af996d4549127a8d08485b6737e1bc8f64cd
Andrew Campbell <andrewcampbell91@gmail.com>
2022-11-14 07:24:00 -0400

    Added documentation

--- Contract.sol
+++ Contract.sol
@@ -1,10 +1,12 @@
 pragma solidity  >=0.5.0 <0.6.0;
 
+//Solidity contract to create Zombies with random DNA
 contract ZombieFactory {
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
+    //Main Zombie "object" - called a "struct" in Solidity
     struct Zombie {
         string name;
         uint dna;
@@ -12,15 +14,18 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
+    //Creates a new Zombie from name and dna
     function _createZombie(string memory _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
     }
 
+    //Uses keccak256 to create random DNA of 16 characters
     function _generateRandomDna(string memory _str) private view returns (uint) {
         uint rand = uint(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
     }
 
+    //Creates a Zombie struct with random DNA
     function createRandomZombie(string memory _name) public {
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
