ca4ffa4e5757efffc114a898e45b69ca35d43f50
Andrew Campbell <andrewcampbell91@gmail.com>
2022-11-19 08:09:19 -0400

    //Updated _createZombie function to map ownership

--- Contract.sol
+++ Contract.sol
@@ -17,12 +17,14 @@ contract ZombieFactory {
     Zombie[] public zombies;
 
     //Added mapping to store Zombie ownership
-    mapping (uint => address) public zombieToOwner;   //keeps track of the address that owns a zombie
-    mapping (address => uint) ownerZombieCount;   //keeps track of how many zombies an owner has
+    mapping (uint => address) public zombieToOwner;    //keeps track of the address that owns a zombie
+    mapping (address => uint) ownerZombieCount;    //keeps track of how many zombies an owner has
 
     //Creates a new Zombie from name and dna
     function _createZombie(string memory _name, uint _dna) private {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;  //pushes new Zombie and returns id in one line
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;   //pushes new Zombie and returns id in one line
+        zombieToOwner[id] = msg.sender;     //maps address that called the function to the id for ownership
+        ownerZombieCount[msg.sender]++;     //maps zombie count + 1 to address that called the function
         emit NewZombie(id, _name, _dna);
     }
 
