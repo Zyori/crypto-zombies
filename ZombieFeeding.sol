ba1c1bd7312530dd1b3ff88c36e981d34b180b92
Andrew Campbell <andrewcampbell91@gmail.com>
2022-11-27 07:00:46 -0400

    Updated file names and added import to zombiefeeding contract

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -1,6 +1,8 @@
-pragma solidity  >=0.5.0 <0.6.0;
+pragma solidity >=0.5.0 <0.6.0;
 
-//Second solidity contract that inherets properties from ZomebieFactory
+//Importts other contract for inheritence
+import "./zombiefactory.sol";
+
 contract ZombieFeeding is ZombieFactory {
 
 }
